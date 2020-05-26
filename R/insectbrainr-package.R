c9f1c7e63e92b7473e5bae44afe126529e3633fb
Gregory Jefferis <jefferis@gmail.com>
2020-05-26 08:58:37 +0100

    Add package documentation with link to pkgdown site

---
+++ R/insectbrainr-package.R
@@ -0,0 +1,10 @@
+#' See \url{http://natverse.org/insectbrainr/} for structured package
+#'   documentation.
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
